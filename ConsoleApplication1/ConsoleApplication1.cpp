2713081ea9eb14f5ec84c7d31738b0418bcf541f
MrJ0nik <142501317+MrJ0nik@users.noreply.github.com>
2024-11-13 23:13:30 +0200

    first commit

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,202 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <string>
+#include <algorithm>
+#include <Windows.h>
+
+using namespace std;
+
+enum Specialization { COMPUTER_SCIENCE, INFORMATICS, MATH_ECONOMICS, PHYSICS_INFORMATICS, LABOR_EDUCATION };
+string specializationStr[] = { "Комп'ютерні науки", "Інформатика", "Математика та економіка", "Фізика та інформатика", "Трудове навчання" };
+
+struct Student {
+    string surname;
+    unsigned course;
+    Specialization specialization;
+    int physics;
+    int mathematics;
+    union {
+        int programming;      
+        int numerical_methods; 
+        int pedagogy;          
+    };
+};
+
+
+void InputStudents(Student* students, int N);
+void PrintStudents(const Student* students, int N);
+int GetProfileGrade(const Student& student);
+void PhysicalSort(Student* students, int N);
+void IndexSort(const Student* students, int N, int* indexArray);
+int BinarySearch(const Student* students, const int* indexArray, int N, const string& surname, int course, int profileGrade);
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int numStudents;
+    cout << "Введіть кількість студентів: ";
+    cin >> numStudents;
+
+    Student* students = new Student[numStudents];
+    int* indexArray = new int[numStudents];
+
+    int menuItem;
+    do {
+        cout << "\nМеню:\n";
+        cout << "1 - Введення даних студентів\n";
+        cout << "2 - Вивід даних студентів\n";
+        cout << "3 - Фізичне сортування студентів\n";
+        cout << "4 - Індексне сортування студентів\n";
+        cout << "5 - Пошук студента\n";
+        cout << "0 - Вихід\n";
+        cout << "Виберіть дію: ";
+        cin >> menuItem;
+
+        switch (menuItem) {
+        case 1:
+            InputStudents(students, numStudents);
+            break;
+        case 2:
+            PrintStudents(students, numStudents);
+            break;
+        case 3:
+            PhysicalSort(students, numStudents);
+            cout << "Список студентів фізично відсортовано.\n";
+            break;
+        case 4:
+            IndexSort(students, numStudents, indexArray);
+            cout << "Список студентів індексно відсортовано.\n";
+            break;
+        case 5: {
+            string surname;
+            int course, profileGrade;
+            cout << "Введіть прізвище для пошуку: ";
+            cin >> surname;
+            cout << "Введіть курс для пошуку: ";
+            cin >> course;
+            cout << "Введіть оцінку з профільного предмету для пошуку: ";
+            cin >> profileGrade;
+
+            int result = BinarySearch(students, indexArray, numStudents, surname, course, profileGrade);
+            if (result != -1)
+                cout << "Студент знайдений за індексом: " << result << "\n";
+            else
+                cout << "Студент не знайдений.\n";
+            break;
+        }
+        case 0:
+            break;
+        default:
+            cout << "Неправильний вибір. Спробуйте ще раз.\n";
+        }
+    } while (menuItem != 0);
+
+    delete[] students;
+    delete[] indexArray;
+    return 0;
+}
+
+
+void InputStudents(Student* students, int N) {
+    int spec;
+    for (int i = 0; i < N; i++) {
+        cout << "Студент № " << i + 1 << ":\n";
+        cin.ignore();
+        cout << " Прізвище: "; getline(cin, students[i].surname);
+        cout << " Курс: "; cin >> students[i].course;
+        cout << " Спеціальність (0 - Комп'ютерні науки, 1 - Інформатика, 2 - Математика та економіка, 3 - Фізика та інформатика, 4 - Трудове навчання): ";
+        cin >> spec;
+        students[i].specialization = static_cast<Specialization>(spec);
+        cout << " Оцінка з фізики: "; cin >> students[i].physics;
+        cout << " Оцінка з математики: "; cin >> students[i].mathematics;
+
+      
+        if (students[i].specialization == COMPUTER_SCIENCE) {
+            cout << " Оцінка з програмування: ";
+            cin >> students[i].programming;
+        }
+        else if (students[i].specialization == INFORMATICS) {
+            cout << " Оцінка з чисельних методів: ";
+            cin >> students[i].numerical_methods;
+        }
+        else {
+            cout << " Оцінка з педагогіки: ";
+            cin >> students[i].pedagogy;
+        }
+    }
+}
+
+
+void PrintStudents(const Student* students, int N) {
+
+
+    cout << "=========================================================================================================\n";
+    cout << "| № | Прізвище           | Курс | Спеціальність           | Фізика | Математика  | Профільний предмет   |\n";
+    cout << "---------------------------------------------------------------------------------------------------------\n";
+
+
+    for (int i = 0; i < N; i++) {
+
+        int profileGrade = GetProfileGrade(students[i]);
+        cout << setw(2) << left << "| " << i + 1 << " | " << setw(18)  << students[i].surname << " | " << setw(4) << students[i].course << " | ";
+        cout << setw(21) << left << specializationStr[students[i].specialization] << " | " << setw(6) << students[i].physics << " | ";
+        cout << setw(11) << students[i].mathematics << " | " << setw(20) << profileGrade << " |\n";
+    }
+    cout << "=========================================================================================================\n";
+}
+
+
+int GetProfileGrade(const Student& student) {
+    if (student.specialization == COMPUTER_SCIENCE) return student.programming;
+    else if (student.specialization == INFORMATICS) return student.numerical_methods;
+    else return student.pedagogy;
+}
+
+
+void PhysicalSort(Student* students, int N) {
+    sort(students, students + N, [](const Student& a, const Student& b) {
+        int gradeA = GetProfileGrade(a);
+        int gradeB = GetProfileGrade(b);
+        if (gradeA != gradeB) return gradeA > gradeB;
+        if (a.course != b.course) return a.course < b.course;
+        return a.surname > b.surname;
+        });
+}
+
+
+void IndexSort(const Student* students, int N, int* indexArray) {
+    for (int i = 0; i < N; i++) {
+        indexArray[i] = i;
+    }
+    sort(indexArray, indexArray + N, [&](int a, int b) {
+        int gradeA = GetProfileGrade(students[a]);
+        int gradeB = GetProfileGrade(students[b]);
+        if (gradeA != gradeB) return gradeA > gradeB;
+        if (students[a].course != students[b].course) return students[a].course < students[b].course;
+        return students[a].surname < students[b].surname;
+        });
+}
+
+
+int BinarySearch(const Student* students, const int* indexArray, int N, const string& surname, int course, int profileGrade) {
+    int left = 0, right = N - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        int idx = indexArray[mid];
+        const Student& student = students[idx];
+        int studentGrade = GetProfileGrade(student);
+
+        if (student.surname == surname && student.course == course && studentGrade == profileGrade) {
+            return idx;
+        }
+        else if (studentGrade < profileGrade || (studentGrade == profileGrade && student.course > course) ||
+            (studentGrade == profileGrade && student.course == course && student.surname < surname)) {
+            right = mid - 1;
+        }
+        else {
+            left = mid + 1;
+        }
+    }
+    return -1;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,72 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../ConsoleApplication1/ConsoleApplication1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+    TEST_CLASS(UnitTest1)
+    {
+    public:
+
+        TEST_METHOD(TestGetProfileGrade)
+        {
+            Student student1 = { "Ivanov", 1, COMPUTER_SCIENCE, 90, 80, 85 };
+            Student student2 = { "Petrov", 2, INFORMATICS, 75, 85, 90 };
+            Student student3 = { "Sidorov", 3, LABOR_EDUCATION, 70, 90, 95 };
+
+            Assert::AreEqual(85, GetProfileGrade(student1));
+            Assert::AreEqual(90, GetProfileGrade(student2));
+            Assert::AreEqual(95, GetProfileGrade(student3));
+        }
+
+        TEST_METHOD(TestPhysicalSort)
+        {
+            Student students[3] = {
+                { "Ivanov", 2, COMPUTER_SCIENCE, 90, 80, 85 },
+                { "Petrov", 1, INFORMATICS, 75, 85, 90 },
+                { "Sidorov", 3, LABOR_EDUCATION, 70, 90, 95 }
+            };
+
+            PhysicalSort(students, 3);
+
+            Assert::AreEqual(std::string("Sidorov"), students[0].surname);
+            Assert::AreEqual(std::string("Petrov"), students[1].surname);
+            Assert::AreEqual(std::string("Ivanov"), students[2].surname);
+        }
+
+        TEST_METHOD(TestIndexSort)
+        {
+            Student students[3] = {
+                { "Ivanov", 2, COMPUTER_SCIENCE, 90, 80, 85 },
+                { "Petrov", 1, INFORMATICS, 75, 85, 90 },
+                { "Sidorov", 3, LABOR_EDUCATION, 70, 90, 95 }
+            };
+            int indexArray[3] = { 0, 1, 2 };
+
+            IndexSort(students, 3, indexArray);
+
+            Assert::AreEqual(std::string("Sidorov"), students[indexArray[0]].surname);
+            Assert::AreEqual(std::string("Petrov"), students[indexArray[1]].surname);
+            Assert::AreEqual(std::string("Ivanov"), students[indexArray[2]].surname);
+        }
+
+        TEST_METHOD(TestBinarySearch)
+        {
+            Student students[3] = {
+                { "Ivanov", 2, COMPUTER_SCIENCE, 90, 80, 85 },
+                { "Petrov", 1, INFORMATICS, 75, 85, 90 },
+                { "Sidorov", 3, LABOR_EDUCATION, 70, 90, 95 }
+            };
+            int indexArray[3] = { 0, 1, 2 };
+            IndexSort(students, 3, indexArray);
+
+            int result = BinarySearch(students, indexArray, 3, "Petrov", 1, 90);
+            Assert::AreEqual(1, result);
+
+            result = BinarySearch(students, indexArray, 3, "Ivanov", 2, 85);
+            Assert::AreEqual(0, result);
+        }
+    };
+}
